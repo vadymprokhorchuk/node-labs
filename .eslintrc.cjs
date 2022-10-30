76f5908c0db9519b2b603964de354e84a82da325
Vadym <ia03.prokhorchuk.vadym@gmail.com>
2022-10-30 18:20:26 +0200

    fix eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true,
-    },
-    extends: ["standard", "plugin:sonarjs/recommended", "prettier"],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: ["sonarjs"],
-    rules: {},
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard", "plugin:sonarjs/recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["sonarjs"],
+  rules: {},
 };
