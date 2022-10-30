d266c4d1ba7a129dae3afa3aebb69cb9d9d3248c
Vadym <ia03.prokhorchuk.vadym@gmail.com>
2022-10-30 15:23:13 +0200

    run lint:fix & format:fix

--- prettierrc.js
+++ prettierrc.js
@@ -1,7 +1,7 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  trailingComma: "es6",
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
+    trailingComma: "es6",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
 };
